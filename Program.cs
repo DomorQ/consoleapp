064bf2f4cc169a1902399090ac4235389ddbe4f9
Jorge Dominguez <domorqfut1998@gmail.com>
2022-06-03 18:12:45 -0500

    aplicacion en consola .NET

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Humanizer;
+
+Console.WriteLine("Por favor, ingrese un nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Ingrese su cargo:");
+var cargo = Console.ReadLine();
+Console.WriteLine("Ingrese su edad:");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Hola, Mi nombre es: {nombre}, mi cargo es: {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años. ");
